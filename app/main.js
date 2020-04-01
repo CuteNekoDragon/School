483d531105888d4be725757784ee12a65a09d6a4
Flonja <vanjavanessen@icloud.com>
2020-04-01 19:37:03 +0200

    Commands api (soort van)

--- app/main.js
+++ app/main.js
@@ -10,19 +10,28 @@ client.on('ready', () => {
 	client.user.setPresence({ activity: { name: 'jouw huiswerk', type: 'WATCHING' }, status: 'online' });
 });
 
+client.on('message', (msg) => {
+	if(msg.author.bot) return;
+	if(!msg.guild) return;
+	
+	if(msg.content.startsWith(config.prefix)) {
+		let command = msg.content.substring(config.prefix.length, msg.content.indexOf(' ') !== -1 ? msg.content.indexOf(' ') : msg.content.length);
+		let args = msg.content.indexOf(' ') !== -1 ? msg.content.substring(config.prefix.length + command.length + 1).split(' ') : [];
+		
+		logger(msg.author.tag + ': ' + msg.content);
+	}
+});
+
 function start(token, log) {
 	logger = log;
 	client.login(token);
 }
-
 function stop() {
 	client.user.setPresence({ activity: { name: 'met nieuwe updates', type: 'PLAYING' }, status: 'dnd' });
 }
-
 function terminate() {
 	client.destroy();
 }
-
 module.exports = {
 	start: start,
 	stop: stop,
