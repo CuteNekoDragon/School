9296e065376dae5736165b33ec136ff82af759f4
pj555pj <61591734+pj555pj@users.noreply.github.com>
2020-04-09 13:02:48 +0200

    Add files via upload
    if you type createchannel it will create test chanel i havent testet it it probebly will fail

---
+++ main.1.js
@@ -0,0 +1,61 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+
+const client = new Discord.Client();
+
+/**
+	logger(str): console.log(`[DISCORD]: ${str}`);
+	config: JSON.parse(fs.readFileSync(`${$workingDir}/../config.json`));
+	db: {
+		read(part, reason): (example: db.read('admins[0].name', `${user} had de naam nodig`))
+		write(part, value, reason): (example: db.write('admins[1].email', 'joost@gmail.com', 'Hij heeft een andere email'))
+	}
+	workingDir: './app/'
+**/
+let logger, config, db, workingDir;
+
+client.on('ready', () => {
+	logger(`Logged in as ${client.user.tag}!`);
+	client.user.setPresence({ activity: { name: 'jouw huiswerk', type: 'WATCHING' }, status: 'online' });
+});
+
+client.on('message', (msg) => {
+	if(msg.author.bot) return;
+	if(!msg.guild) return;	
+
+	if(message.content.startsWith(`${prefix}!createchannel`)) {
+		const args = message.content.slice(15);
+		message.guild.createChannel(`${args}`).then(channel => {
+			channel.setTopic(`test chanel`)
+		})
+	}
+
+	if(msg.content.startsWith(config.prefix)) {
+		let command = msg.content.substring(config.prefix.length, msg.content.indexOf(' ') !== -1 ? msg.content.indexOf(' ') : msg.content.length);
+		let args = msg.content.indexOf(' ') !== -1 ? msg.content.substring(config.prefix.length + command.length + 1).split(' ') : [];
+
+		logger(msg.author.tag + ': ' + msg.content);
+		msg.channel.send(msg.author.tag + ': ' + msg.content);
+	}
+});
+
+function start(token, args) {
+	logger = args.logger;
+	config = args.config;
+	db = args.db;
+	workingDir = args.dir;
+	
+	client.login(token);
+}
+
+function stop() {
+	client.user.setPresence({ activity: { name: 'met nieuwe updates', type: 'PLAYING' }, status: 'dnd' });
+}
+function terminate() {
+	client.destroy();
+}
+module.exports = {
+	start: start,
+	stop: stop,
+	terminate: terminate
+}
